58e413e1600dbe9b500c66edd28740361071b99a
Kyshima <57459633+Kyshima@users.noreply.github.com>
2024-03-11 22:17:39 +0000

    largura e altura

--- Dataset-Preparation/ReconstrucaoCSV.py
+++ Dataset-Preparation/ReconstrucaoCSV.py
@@ -4,14 +4,14 @@ import cv2
 import pandas as pd
 import pickle
 
-def reconstruir_imagem(caminho_csv, indice):
+def reconstruir_imagem(caminho_csv, indice, largura, altura):
     df = pd.read_csv(caminho_csv)
 
     linha = df.iloc[indice]
     nome_da_imagem = 'Teste.jpg'
 
     informacao_pixels = pickle.loads(eval(linha['Informacao de Pixels']))
-    imagem_reconstruida = np.array(informacao_pixels, dtype=np.uint8).reshape(128, 128, 3)
+    imagem_reconstruida = np.array(informacao_pixels, dtype=np.uint8).reshape(largura, altura, 3)
 
     caminho_saida = os.path.join(f"{nome_da_imagem}")
     print(caminho_saida)
@@ -19,5 +19,6 @@ def reconstruir_imagem(caminho_csv, indice):
 
 if __name__ == "__main__":
     caminho_csv = 'DatasetBinary128.csv'
-    indice_a_reconstruir = 10  # índice da linha a reconstruir
-    reconstruir_imagem(caminho_csv, indice_a_reconstruir)
+    indice_a_reconstruir = 178  # índice da linha a reconstruir
+    largura, altura = 128, 128
+    reconstruir_imagem(caminho_csv, indice_a_reconstruir, largura, altura)
